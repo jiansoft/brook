53e19d2567b174cffc666728b9ee92a26f9160c8
eddie Chen <ez3608@yahoo.com.tw>
2017-09-15 23:38:12 +0800

    1.Add Method Bulk (SqlBulkCopy)

--- Brook/MsSql.cs
+++ Brook/MsSql.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using jIAnSoft.Framework.Brook.Configuration;
+using jIAnSoft.Framework.Brook.Utility;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.Common;
+using System.Data.SqlClient;
 using System.Globalization;
-using jIAnSoft.Framework.Brook.Configuration;
-using jIAnSoft.Framework.Brook.Utility;
 
 namespace jIAnSoft.Framework.Brook
 {
@@ -100,7 +101,7 @@ namespace jIAnSoft.Framework.Brook
         {
             return First<T>(Timeout, commandType, sqlCmd, parameters);
         }
-        
+
         /// <summary>
         /// 執行查詢後只回傳第一列的資料 V2.0
         /// </summary>
@@ -458,6 +459,42 @@ namespace jIAnSoft.Framework.Brook
             finally
             {
                 QueryCompleted();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="batchSize"></param>
+        public void Bulk(DataTable table, int batchSize = 2500)
+        {
+            using (var bulk = new SqlBulkCopy(ConnectionSource))
+            {
+                bulk.BatchSize = batchSize;
+                bulk.BulkCopyTimeout = 0;
+                bulk.DestinationTableName = $"[dbo].[{table.TableName}]";
+                foreach (DataColumn column in table.Columns)
+                {
+                    bulk.ColumnMappings.Add(column.ColumnName, column.ColumnName);
+                }
+                try
+                {
+                    // Write from the source to the destination.
+                    bulk.WriteToServer(table);
+                }
+                catch (Exception sqlEx)
+                {
+                    throw new SqlException(
+                        string.Format(
+                            new CultureInfo(Section.Get.Common.Culture.Name),
+                            "{0} Source = {1}\n Table = {2}\n",
+                            sqlEx.Message,
+                            DbConfig.Name,
+                            table.TableName
+                        ),
+                        sqlEx);
+                }
             }
         }
     }
